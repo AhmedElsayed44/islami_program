7e000d407d4cc196702d018bd04d23a6241801d5
AhmedElsayed <ae234918@gmail.com>
2021-11-01 20:11:24 +0200

    added hades screen

---
+++ lib/home/hades/hades_ditals_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:newflutter/main.dart';
+import 'hades_tap.dart';
+
+class hadesditals extends StatefulWidget {
+  static const String routName = 'hadesditals';
+
+  @override
+  _hadesditalsState createState() => _hadesditalsState();
+}
+
+class _hadesditalsState extends State<hadesditals> {
+  List<String> hades = [];
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as hadesditalsargs;
+    loadhadesditals(args.hadesIndex);
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/image/main_background.png',
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              '${args.hadesName}',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            centerTitle: true,
+          ),
+          body: Container(
+              margin: EdgeInsets.symmetric(vertical: 40,horizontal: 24),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+              child: hades.isEmpty
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.separated(
+                separatorBuilder: (buildcotext,index){
+                  return Container(
+                    height: 1,
+                    width: double.infinity,
+                    color: mythemdata.primaryColor,
+                  );
+                },
+                itemBuilder: (buildcontext, index) {
+                  return Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        hades[index]+'{${index+1}}',
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.rtl,
+                      ),
+                    ),
+                  );
+                },
+                itemCount: hades.length-1,
+              )),
+        ),
+      ],
+    );
+  }
+
+  void loadhadesditals(int index) async {
+    String filecontent =
+    await rootBundle.loadString('assets/file/${index + 1}.txt');
+    List<String> hadescontent = filecontent.split('\n');
+    this.hades = hadescontent;
+    setState(() {
+
+    });
+
+  }
+}
+
+class hadesditalsargs {
+  String hadesName;
+  int hadesIndex;
+  hadesditalsargs(this.hadesName, this.hadesIndex);
+}
--- lib/home/quran/quran_tap.dart
+++ lib/home/quran/quran_tap.dart
@@ -1,11 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:newflutter/home/quran/sura_name_item.dart';
+import 'package:newflutter/main.dart';
 
 class quran_tap extends StatelessWidget{
+  final List<String>  suranames=["الفاتحه","البقرة","آل عمران","النساء","المائدة","الأنعام","الأعراف","الأنفال","التوبة","يونس","هود"
+  ,"يوسف","الرعد","إبراهيم","الحجر","النحل","الإسراء","الكهف","مريم","طه","الأنبياء","الحج","المؤمنون"
+  ,"النّور","الفرقان","الشعراء","النّمل","القصص","العنكبوت","الرّوم","لقمان","السجدة","الأحزاب","سبأ"
+  ,"فاطر","يس","الصافات","ص","الزمر","غافر","فصّلت","الشورى","الزخرف","الدّخان","الجاثية","الأحقاف"
+  ,"محمد","الفتح","الحجرات","ق","الذاريات","الطور","النجم","القمر","الرحمن","الواقعة","الحديد","المجادلة"
+  ,"الحشر","الممتحنة","الصف","الجمعة","المنافقون","التغابن","الطلاق","التحريم","الملك","القلم","الحاقة","المعارج"
+  ,"نوح","الجن","المزّمّل","المدّثر","القيامة","الإنسان","المرسلات","النبأ","النازعات","عبس","التكوير","الإنفطار"
+  ,"المطفّفين","الإنشقاق","البروج","الطارق","الأعلى","الغاشية","الفجر","البلد","الشمس","الليل","الضحى","الشرح"
+  ,"التين","العلق","القدر","البينة","الزلزلة","العاديات","القارعة","التكاثر","العصر",
+  "الهمزة","الفيل","قريش","الماعون","الكوثر","الكافرون","النصر","المسد","الإخلاص","الفلق","الناس"];
   @override
   Widget build(BuildContext context) {
-   return Container(
-     color: Colors.red,
+   return Column(
+     crossAxisAlignment: CrossAxisAlignment.center,
+     children:[ Expanded(
+         flex: 1,
+         child: Image.asset('assets/image/logo_home.png')
+     ),
+       Container(
+         height: 1,
+         width: double.infinity,
+         color: mythemdata.primaryColor,
+       ),
+       Padding(
+         padding: const EdgeInsets.all(8.0),
+         child: Text('sura name',style: TextStyle(fontSize: 25),),
+       ),
+       Container(
+         height: 1,
+         width: double.infinity,
+         color: mythemdata.primaryColor,
+       ),
+       Expanded(
+           flex: 3,
+           child: Container(
+             child:ListView.separated
+               (separatorBuilder: (buildcotext,index){
+                 return Container(
+                   height: 1,
+                   width: double.infinity,
+                   color: mythemdata.primaryColor,
+                 );
+             },
+
+               itemBuilder: (buildcotext,index){
+               return suranameitem(suranames[index], index);
+             },itemCount: suranames.length,
+
+
+
+             )
+
+           )
+       )
+     ],
    );
+
+
+
   }
 }
